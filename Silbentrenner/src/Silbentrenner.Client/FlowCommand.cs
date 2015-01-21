c9a7001567d6abfe04d4ecef5eba3b9e422cfef3
sbetzin <lovegirl1>
2015-01-21 13:43:33 +0100

    UI Implementierung

---
+++ Silbentrenner/src/Silbentrenner.Client/FlowCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Silbentrenner.Client
+{
+    public class FlowCommand : ICommand
+    {
+        private readonly Func<bool> m_CanExecute;
+        public event Action Executed;
+
+        public event EventHandler CanExecuteChanged;
+
+        public FlowCommand() { }
+
+        public FlowCommand(Func<bool> CanExecute)
+        {
+            m_CanExecute = CanExecute;
+        }
+
+        public bool CanExecute(object Parameter)
+        {
+            if (m_CanExecute == null)
+            {
+                return true;
+            }
+
+            return m_CanExecute();
+        }
+
+        public void CheckCanExecute()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged.Invoke(this, new EventArgs());
+            }
+        }
+
+        public async void Execute(object Parameter)
+        {
+            OnExecuted();
+        }
+
+        protected virtual void OnExecuted()
+        {
+            var Handler = Executed;
+            if (Handler != null)
+            {
+                Handler();
+            }
+        }
+
+    }
+}
