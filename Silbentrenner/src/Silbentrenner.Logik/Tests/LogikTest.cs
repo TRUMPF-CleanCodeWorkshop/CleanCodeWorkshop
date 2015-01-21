ddbf74348718f518b29cf7799da167621767ce30
Daniel Maier <daniel.maier@de.trumpf.com>
2015-01-21 12:05:56 +0100

    [TASK] Testordner hinzugefügt

---
+++ Silbentrenner/src/Silbentrenner.Logik/Tests/LogikTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace Silbentrenner.Logik
+{
+    [TestFixture]
+    public class LogikTest
+    {
+
+        [Test]
+        public void ErsetzeCRLFDurchSpace_zwei_Returns()
+        {
+            var text = "123" + Environment.NewLine + "ab" + Environment.NewLine;
+
+
+
+        }
+
+
+    }
+}
