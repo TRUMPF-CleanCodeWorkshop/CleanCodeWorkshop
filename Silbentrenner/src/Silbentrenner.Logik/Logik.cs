deb23158aee9b22926de18a2c28418d6834113be
Sebastien-Claude Mordellet <sebastien-claude.mordellet@de.trumpf.com>
2015-01-21 11:58:26 +0100

    [TASK] Methodenruempfe von Team2

--- Silbentrenner/src/Silbentrenner.Logik/Logik.cs
+++ Silbentrenner/src/Silbentrenner.Logik/Logik.cs
@@ -8,5 +8,14 @@ namespace Silbentrenner.Logik
 {
     public class Logik
     {
+        public static string BereinigenVonLeerzeichen(string text)
+        {
+            return "";
+        }
+
+        public static IEnumerable<Wort> WoerterInSilbenTrennen(IEnumerable<Wort> woerter)
+        {
+            return new List<Wort>();
+        }
     }
 }
