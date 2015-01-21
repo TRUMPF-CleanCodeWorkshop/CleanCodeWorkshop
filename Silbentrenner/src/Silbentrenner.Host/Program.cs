48f7308a6ede43c584a2f85941fecf3993c35fe0
sbetzin <lovegirl1>
2015-01-21 11:53:56 +0100

    Host Settings

--- Silbentrenner/src/Silbentrenner.Host/Program.cs
+++ Silbentrenner/src/Silbentrenner.Host/Program.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Silbentrenner.Client;
 
 namespace Silbentrenner.Host
 {
     
+
     public class Program
     {
+        [STAThread]
         public static void Main(string[] args)
         {
+            var mainWindow = new MainWindow();
+
+            mainWindow.DataContext = new MainViewModel();
+
+            mainWindow.Show();
+
+            var app = new App();
+            app.
+           
         }
     }
 }
