feaa700859fb9d42f8f15e02c8a4bdcb5c10bcd1
Daniel Maier <daniel.maier@de.trumpf.com>
2015-01-21 14:35:45 +0100

    [TASK] WoerterZuZeilenZusammensetzten ergänzt

--- Silbentrenner/src/Silbentrenner.Logik/Logik.cs
+++ Silbentrenner/src/Silbentrenner.Logik/Logik.cs
@@ -16,7 +16,32 @@ namespace Silbentrenner.Logik
 
         public static IEnumerable<String> WoerterZuZeilenZusammensetzen(IEnumerable<Wort> woerter, int zeilenlaenge)
         {
-            return new List<String>();
+            var aktuelleZeile = "";
+            var vorigeSilbeWarLetzteDesWortes = false;
+
+            foreach (var wort in woerter)
+            {
+                foreach (var silbe in wort.Silben)
+                {
+                    if ((aktuelleZeile.Length + silbe.Length) >= zeilenlaenge)
+                    {
+                        yield return VollstaendigeZeileZurueckgeben(vorigeSilbeWarLetzteDesWortes, ref aktuelleZeile);
+                    }
+                    aktuelleZeile += silbe;
+                    vorigeSilbeWarLetzteDesWortes = wort.Silben.Last() == silbe;
+                }
+                aktuelleZeile += " ";
+            }
+            aktuelleZeile = aktuelleZeile.Trim();
+            yield return aktuelleZeile;
+        }
+
+        private static string VollstaendigeZeileZurueckgeben(bool vorigeSilbeWarLetzteDesWortes, ref string aktuelleZeile)
+        {
+            aktuelleZeile = aktuelleZeile.Trim();
+            if (!vorigeSilbeWarLetzteDesWortes) aktuelleZeile += "-";
+            return aktuelleZeile;
+            aktuelleZeile = "";
         }
 
         public static IEnumerable<Wort> TextInWoerterZerlegen(string text)
