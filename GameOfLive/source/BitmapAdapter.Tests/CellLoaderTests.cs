691d73e65d4a6138da5e8be3a445bc0f14822925
lukasra92 <lukas.raschke@web.de>
2015-03-11 15:04:53 +0100

    [TASK] Test class

---
+++ GameOfLive/source/BitmapAdapter.Tests/CellLoaderTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitmapAdapter.Tests
+{
+    using System.Drawing;
+    using System.IO;
+
+    using GameOfLife.BitmapAdapter;
+
+    using NUnit.Framework;
+
+    public class CellLoaderTests
+    {
+        [Test]
+        public void LoadBitmap_Load_file_is_successfull()
+        {
+            var path = @"..\..\glider.gif";
+
+            var bitmap = CellLoader.LoadBitmap(path);
+
+            Assert.That(bitmap, !Is.Null);
+        }
+
+        [Test]
+        public void LoadBitmap_3_x_3_File_returns_3_x_3_Bitmap_Object()
+        {
+            var path = @"..\..\glider.gif";
+
+            var bitmap = CellLoader.LoadBitmap(path);
+
+            Assert.That(bitmap.Size, Is.EqualTo(new Size(3, 3)));
+        }
+
+        [Test]
+        public void CellLoader_glider_gives_correct_points()
+        {
+            var path = @"..\..\glider.gif";
+
+            var bitmap = CellLoader.LoadBitmap(path);
+
+
+            var cellList = CellLoader.CalculateInitalCells(bitmap, new Size(3, 3));
+
+            Assert.That(cellList.Contains(new Point(0, 1)) && cellList.Contains(new Point(2, 2)) && !cellList.Contains(new Point(1, 1)));
+        }
+
+        [Test]
+        public void CellLoader_glider_is_placed_in_correct_position()
+        {
+            var path = @"..\..\glider.gif";
+
+            var bitmap = CellLoader.LoadBitmap(path);
+
+            var cellList = CellLoader.CalculateInitalCells(bitmap, new Size(500, 500));
+
+            Assert.That(
+                cellList.Contains(new Point(249, 250)) && cellList.Contains(new Point(251, 251))
+                && !cellList.Contains(new Point(249, 249)));
+        }
+    }
+}
