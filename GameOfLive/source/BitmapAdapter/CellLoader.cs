43daa6af819f5b51bef7e7b3659959d8edabcb77
sbetzin <lovegirl1>
2015-03-11 13:36:21 +0100

    Erste Funktionalität in der GameLogic

---
+++ GameOfLive/source/BitmapAdapter/CellLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using GameOfLife.Contracts;
+
+namespace GameOfLife.BitmapAdapter
+{
+    public class CellLoader
+    {
+        public static Cells Load(string path, Size mapSize)
+        {
+            var bitmap = LoadBitmap(path);
+
+            return CalculateInitalCells(bitmap, mapSize);
+        }
+
+        internal static Bitmap LoadBitmap(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Cells CalculateInitalCells(Bitmap bitmap, Size mapSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameOfLive/source/Contracts/Cells.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace GameOfLife.Contracts
+{
+    public class Cells : List<Point>
+    {
+        public int Population { get; set; }
+        public int Generation { get; set; }
+    }
+}
