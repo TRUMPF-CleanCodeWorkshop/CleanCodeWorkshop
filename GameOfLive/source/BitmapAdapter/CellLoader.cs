cd5be0eff74801318468ca27de6b95d01455d2aa
lukasra92 <lukas.raschke@web.de>
2015-03-11 14:58:50 +0100

    [FEATURE] File adapter

--- GameOfLive/source/BitmapAdapter/CellLoader.cs
+++ GameOfLive/source/BitmapAdapter/CellLoader.cs
@@ -4,6 +4,9 @@ using GameOfLife.Contracts;
 
 namespace GameOfLife.BitmapAdapter
 {
+    using System.IO;
+    using System.Runtime.InteropServices;
+
     public class CellLoader
     {
         public static Cells Load(string path, Size mapSize)
@@ -15,12 +18,29 @@ namespace GameOfLife.BitmapAdapter
 
         internal static Bitmap LoadBitmap(string path)
         {
-            throw new NotImplementedException();
+            // var fileContent = File.ReadAllBytes(path);
+            return new Bitmap(Bitmap.FromFile(path));
         }
 
         internal static Cells CalculateInitalCells(Bitmap bitmap, Size mapSize)
         {
-            throw new NotImplementedException();
+            var cells = new Cells() { Generation = 0, Population = 0 };
+            var bitmapSize = bitmap.Size;
+            var startingPosition = new Point(mapSize.Width / 2 - bitmapSize.Width / 2, mapSize.Height / 2 - bitmapSize.Height / 2);
+
+            for (var widthCount = 0; widthCount < bitmapSize.Width; widthCount++)
+            {
+                for (var heightCount = 0; heightCount < bitmapSize.Height; heightCount++)
+                {
+                    var pixel = bitmap.GetPixel(widthCount, heightCount);
+                    if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0)
+                    {
+                        cells.Add(new Point(startingPosition.X + widthCount, startingPosition.Y + heightCount));
+                    }
+                }
+            }
+
+            return cells;
         }
     }
 }
