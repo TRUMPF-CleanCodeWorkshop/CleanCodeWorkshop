fb420da0ce278d05e26117499e4a258c7a8cbc5b
sbetzin <lovegirl1>
2015-04-09 13:31:19 +0200

    Host

---
+++ RobotTournament/source/Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var basePath = GetBasePath();
+            var gameEnginePath =  Path.Combine(basePath , "engines");
+
+            var gameEngine = GameEngineAdapter.EngineLoader.Load(gameEnginePath);
+        }
+
+        private static string GetBasePath()
+        {
+            var file = new FileInfo(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+
+            // ReSharper disable once PossibleNullReferenceException
+            return file.Directory.FullName;
+        }
+    }
+}
