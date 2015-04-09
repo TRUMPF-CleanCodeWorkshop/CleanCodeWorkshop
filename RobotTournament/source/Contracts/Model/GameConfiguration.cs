4a76e46b72419fd7f308fe9dea37ddcf67f007b5
sbetzin <lovegirl1>
2015-04-09 13:02:28 +0200

    UI

---
+++ RobotTournament/source/Contracts/Model/GameConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Model
+{
+    public class GameConfiguration
+    {
+        public Size MapSize{ get; set; }
+        public double PowerupPropability { get; set; }
+    }
+}
---
+++ RobotTournament/source/DebugUI/GameUI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts.Model;
+
+namespace DebugUI
+{
+    public static class GameUI
+    {
+        static void ShowGameState(GameState gameState)
+        {
+            
+        }
+    }
+}
