be8187e21c1f73c46eb431cb496bf7fe38fb526e
sbetzin <lovegirl1>
2015-04-09 13:07:57 +0200

    Interfaces

---
+++ RobotTournament/source/Core/Framework.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Contracts.Model;
+
+namespace Core
+{
+    public class Framework
+    {
+        public static GameState CreateInitializeGameState(GameConfiguration configuration, IGameEngine engine, IEnumerable<IRobotEngine> robotEngines)
+        {
+            return new GameState();
+        } 
+    }
+}
