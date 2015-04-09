8c84f38ecdb85fde8772ffe25d7246c10c5e51b3
sbetzin <lovegirl1>
2015-04-09 14:34:01 +0200

    refactored

--- RobotTournament/source/Core/Framework.cs
+++ RobotTournament/source/Core/Framework.cs
@@ -18,7 +18,7 @@
 
             var result = new GameState
             {
-                Configuration = configuration, 
+                Configuration = configuration,
                 Turn = 0,
                 PowerUps = new List<PowerUp>(),
                 Robots = new List<Robot>()
@@ -26,16 +26,11 @@
 
             var robotEngineList = robotEngines.ToList();
 
-            var positions = engine.GetInitialRobotPositions(
-                configuration.MapSize, 
-                robotEngineList.Count()).ToList();
+            var positions = engine.GetInitialRobotPositions(configuration.MapSize, robotEngineList.Count()).ToList();
 
             for (var i = 0; i < robotEngineList.Count; i++)
             {
-                var nextRobot = new Robot(
-                    positions[i],
-                    configuration.RobotStartLevel, 
-                    robotEngineList[i].TeamName);
+                var nextRobot = new Robot(positions[i], configuration.RobotStartLevel, robotEngineList[i].TeamName);
                 result.Robots.Add(nextRobot);
             }
 
