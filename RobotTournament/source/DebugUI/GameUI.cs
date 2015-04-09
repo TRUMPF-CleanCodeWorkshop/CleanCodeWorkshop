5375fdec7d8634ce7ef4f2f8454a90a299f5ee14
lukasra92 <lukas.raschke@web.de>
2015-04-09 14:47:52 +0200

    [TASK] Complete output for game state

--- RobotTournament/source/DebugUI/GameUI.cs
+++ RobotTournament/source/DebugUI/GameUI.cs
@@ -7,11 +7,57 @@ using Contracts.Model;
 
 namespace DebugUI
 {
+    using System.Drawing;
+
     public static class GameUI
     {
+        private const string EmptyRow = "|            ";
+
         public static void ShowGameState(GameState gameState)
         {
+            var numberOfColumns = gameState.Configuration.MapSize.Width;
+            Console.WindowWidth = numberOfColumns * 13 + 5;
+
+            var firstRow = new StringBuilder("  |");
             
+            for (var columnCounter = 0; columnCounter < numberOfColumns; columnCounter++)
+            {
+                firstRow.Append(columnCounter.ToString().PadRight(13));
+            }
+            Console.WriteLine(firstRow);
+
+            for (var rowCounter = 0; rowCounter < gameState.Configuration.MapSize.Height; rowCounter++)
+            {
+                PrintRowWithRobots(rowCounter, numberOfColumns, gameState.Robots);
+            }
+        }
+
+        public static void PrintRowWithRobots(int rowNumber, int mapWidth, IEnumerable<Robot> robots)
+        {
+            var firstRow = new StringBuilder("  |");
+
+            var secondRow = new StringBuilder(string.Format("{0} |", rowNumber));
+
+            var thirdRow = new StringBuilder("  |");
+            for (var columnCounter = 0; columnCounter < mapWidth; columnCounter++)
+            {
+                var robot = robots.FirstOrDefault(r => r.Position == new Point(columnCounter, rowNumber));
+                if (robot != null)
+                {
+                    firstRow.Append(GetRowForLevelAndId(robot));
+                    secondRow.Append(GetRowForName(robot));
+                }
+                else
+                {
+                    firstRow.Append(EmptyRow);
+                    secondRow.Append(EmptyRow);
+                }
+                thirdRow.Append(EmptyRow);
+            }
+
+            Console.WriteLine(firstRow);
+            Console.WriteLine(secondRow);
+            Console.WriteLine(thirdRow);
         }
 
         public static string GetRowForLevelAndId(Robot robot)
@@ -19,6 +65,18 @@ namespace DebugUI
             var levelString = robot.Level.ToString().PadRight(5);
             var idString = robot.Id.PadLeft(5);
             return string.Format("|{0} {1}|", levelString, idString);
+        }
+
+        public static string GetRowForName(Robot robot)
+        {
+            var nameString = robot.TeamName.PadRight(11);
+
+            return nameString.Substring(0, 11);
+        }
+
+        public static string GetBorderRowForRobot()
+        {
+            return ""
         }
     }
 }
--- RobotTournament/source/DebugUITests/GameUITests.cs
+++ RobotTournament/source/DebugUITests/GameUITests.cs
@@ -19,9 +19,9 @@ namespace DebugUITests
         [Test]
         public void RowForIdAndLevelIsGivenForARobot()
         {
-            var robot = new Robot(new Point(0, 0), 42, "SuperTeam", "123");
+            var robot = new Robot(new Point(0, 0), 42, "SuperTeam", "1");
 
-            var expectedOutput = "|42      123|";
+            var expectedOutput = "|42        1|";
             var actualOutput = GameUI.GetRowForLevelAndId(robot);
 
             Assert.That(actualOutput, Is.EqualTo(expectedOutput));
@@ -30,7 +30,23 @@ namespace DebugUITests
         [Test]
         public void RowForRobotTeamNameisValid()
         {
-            var 
+            var robot = new Robot(new Point(0, 0), 42, "SuperTeam", "123");
+
+            var expectedOutput = "SuperTeam  ";
+            var actualOutput = GameUI.GetRowForName(robot);
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void RobotTeamNameWithMoreThanElevenCharactersIsHandledCorrectly()
+        {
+            var robot = new Robot(new Point(0, 0), 42, "DasSuperTeamMitVielenZeichenUndSo", "123");
+
+            var expectedOutput = "DasSuperTea";
+            var actualOutput = GameUI.GetRowForName(robot);
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
         }
     }
 }
