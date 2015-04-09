949080b6435285030c42a21c591401354a562ee5
Verena Glaser <verena.glaser@de.trumpf.com>
2015-04-09 13:57:09 +0200

    Add new Robot classes

---
+++ RobotTournament/source/RobotEngineAdapterTests/RobotEngineLoaderTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotEngineAdapter;
+
+namespace RobotEngineAdapterTests
+{
+    public class RobotEngineLoaderTests
+    {
+        [Test]
+        public void Load_lädt_alle_RobotEngines_aus_mehreren_DLL()
+        {
+            var path = @"C:\Data\Development\Katas\Trumpf CleanCodeWorkshop\RobotTournament\bin\engines";
+
+            var robots = RobotLoader.Load(path);
+
+            Assert.That(robots, Is.Not.Empty);
+
+        } 
+    }
+}
