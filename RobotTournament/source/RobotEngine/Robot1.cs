4e0f0ae66608a8feab8bc4133465c34a6bfb1cba
sbetzin <lovegirl1>
2015-06-11 13:37:30 +0200

    Random Logi

--- RobotTournament/source/RobotEngine/Robot1.cs
+++ RobotTournament/source/RobotEngine/Robot1.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Contracts;
 using Contracts.Model;
 
@@ -10,21 +6,32 @@ namespace RobotEngine
 {
     public class Robot1 : IRobot
     {
+        private int upgrade = 10;
+        private int split = 9;
+        private int move = 8;
+
         public NextRobotTurn DoNextTurn(int currentTurn, int myLevel, Surroundings environment)
         {
             var random = new Random();
             var decisionAction = random.Next(100);
             var decisionDirection = (Directions)random.Next(0, 7);
 
-            if (decisionAction > 80)
+            if (currentTurn == 10) VeryEarlyGameSettings();
+            if (currentTurn == 30) EarlyGameSettings();
+            if (currentTurn == 60) MidGameSettings();
+            if (currentTurn == 100) MidLateGameSettings();
+            if (currentTurn == 200) LateGameSettings();
+
+
+            if (decisionAction > upgrade)
             {
                 return new NextRobotTurn() { NextAction = RobotActions.Upgrading, NextDirection = decisionDirection };
             }
-            else if (decisionAction > 60)
+            else if (decisionAction > split)
             {
                 return new NextRobotTurn() { NextAction = RobotActions.Splitting, NextDirection = decisionDirection };
             }
-            else if (decisionAction > 10)
+            else if (decisionAction > move)
             {
                 return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = decisionDirection };
             }
@@ -32,6 +39,41 @@ namespace RobotEngine
             {
                 return new NextRobotTurn() { NextAction = RobotActions.Idle, NextDirection = decisionDirection };
             }
+        }
+
+        private void VeryEarlyGameSettings()
+        {
+            upgrade = 60;
+            split = 20;
+            move = 5;
+        }
+
+        private void EarlyGameSettings()
+        {
+            upgrade = 80;
+            split = 40;
+            move = 5;
+        }
+
+        private void MidGameSettings()
+        {
+            upgrade = 80;
+            split = 60;
+            move = 5;
+        }
+
+        private void MidLateGameSettings()
+        {
+            upgrade = 95;
+            split = 75;
+            move = 0;
+        }
+
+        private void LateGameSettings()
+        {
+            upgrade = 100;
+            split = 90;
+            move = 0;
         }
     }
 }
