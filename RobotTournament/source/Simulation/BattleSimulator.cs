3d29157478ac6f8f9b30dbe4c7201e0a3ab0fba4
sbetzin <lovegirl1>
2015-06-11 15:46:44 +0200

    Simulator e

---
+++ RobotTournament/source/Simulation/BattleSimulator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Contracts.Model;
+
+namespace Simulation
+{
+    public class BattleSimulator
+    {
+        public SimulationResult Simulate(IGameEngine gameEngine, IEnumerable<IRobotEngine> robotEngines)
+        {
+            return new SimulationResult();
+        }
+    }
+}
