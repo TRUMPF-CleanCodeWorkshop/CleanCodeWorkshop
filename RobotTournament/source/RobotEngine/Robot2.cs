a8048208340a2681a0cc729d1d638af254823737
Sebastien-Claude Mordellet <sebastien-claude.mordellet@de.trumpf.com>
2015-06-11 14:54:07 +0200

    ROBOT IMPERATOR (Team 2)

--- RobotTournament/source/RobotEngine/Robot2.cs
+++ RobotTournament/source/RobotEngine/Robot2.cs
@@ -10,18 +10,31 @@
     {
         public NextRobotTurn DoNextTurn(int currentTurn, int myLevel, Surroundings environment)
         {
-            if (environment.PowerUps.Any())
+            // Darwin!
+            var enemies = environment.Robots.Where(r => r.IsEnemy).ToList();
+            var friends = environment.Robots.Where(r => !r.IsEnemy).ToList();
+            foreach (var victim in friends
+                .SelectMany(alliedRobot => enemies
+                    .Where(victim => (alliedRobot.Direction + 1 == victim.Direction) || (alliedRobot.Direction + 1 == victim.Direction))))
             {
-                var firstPowerUpDirection = environment.PowerUps.First().Direction;
-                return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = firstPowerUpDirection };
+                return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = victim.Direction };
             }
 
+            // Hercules
             if (environment.Robots.Any(r => r.IsEnemy && r.Level < myLevel))
             {
                 var firstPussyBot = environment.Robots.First(r => r.IsEnemy && r.Level < myLevel).Direction;
                 return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = firstPussyBot };
             }
 
+            // Nimmersatt
+            if (environment.PowerUps.Any())
+            {
+                var firstPowerUpDirection = environment.PowerUps.First().Direction;
+                return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = firstPowerUpDirection };
+            }
+
+            // Fitness
             var decisionDirection = currentTurn % 5 == 0 ? Directions.NW : Directions.SW;
 
             if (currentTurn % 3 == 0)
