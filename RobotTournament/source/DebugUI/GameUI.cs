2d34879d13294deef9ddc3700b63d6cabc1373a3
lukasra92 <lukas.raschke@web.de>
2015-06-11 14:46:11 +0200

    Feature: Color in UI

--- RobotTournament/source/DebugUI/GameUI.cs
+++ RobotTournament/source/DebugUI/GameUI.cs
@@ -15,7 +15,10 @@ namespace DebugUI
         private const string UnderlineRow = "___________|";
 
         public static void ShowGameState(GameState gameState)
-        {            
+        {
+
+
+            
             var numberOfColumns = gameState.Configuration.MapSize.Width;
             var numberOfRows = gameState.Configuration.MapSize.Height;
             Console.WindowWidth = numberOfColumns * 12 + 5;
@@ -40,9 +43,10 @@ namespace DebugUI
 
         public static void PrintRowWithRobots(int rowNumber, int mapWidth, IEnumerable<Robot> robots, IEnumerable<PowerUp> powerUps)
         {
-            var firstRow = new StringBuilder("  |");
+            var teams = robots.GroupBy(r => r.TeamName).Select(t => t.First().TeamName).ToList();
 
-            var secondRow = new StringBuilder(string.Format("{0} |", rowNumber));
+            var firstRowStrings = new List<string>() { "  |" };
+            var secondRowStrings = new List<string>() { string.Format("{0} |", rowNumber) };
 
             var thirdRow = new StringBuilder("  |");
 
@@ -53,27 +57,67 @@ namespace DebugUI
                 var robot = robots.FirstOrDefault(r => r.Position == new Point(columnCounter, rowNumber));
                 if (robot != null)
                 {
-                    firstRow.Append(GetRowForLevelAndId(robot));
-                    secondRow.Append(GetRowForName(robot));
+                    firstRowStrings.Add(GetRowForLevelAndId(robot));
+                    secondRowStrings.Add(GetRowForName(robot));
                     thirdRow.Append(GetRowForAction(robot));
-                } else if (powerUp != null)
+                }
+                else if (powerUp != null)
                 {
-                    firstRow.Append(string.Format("{0}|", "PowerUp".PadRight(11)));
-                    secondRow.Append(string.Format("{0}|", powerUp.Level.ToString().PadRight(11)));
+                    firstRowStrings.Add(string.Format("{0}|", "PowerUp".PadRight(11)));
+                    secondRowStrings.Add(string.Format("{0}|", powerUp.Level.ToString().PadRight(11)));
                     thirdRow.Append(EmptyRow);
                 }
                 else
                 {
-                    firstRow.Append(EmptyRow);
-                    secondRow.Append(EmptyRow);
+                    firstRowStrings.Add(EmptyRow);
+                    secondRowStrings.Add(EmptyRow);
                     thirdRow.Append(EmptyRow);
                 }
 
                 fourthRow.Append(UnderlineRow);
             }
 
-            Console.WriteLine(firstRow);
-            Console.WriteLine(secondRow);
+            foreach (var firstrowString in firstRowStrings)
+            {
+                if (firstrowString.Contains("PowerUp"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                Console.Write(firstrowString);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.Write(Environment.NewLine);
+
+            foreach (var secondRowString in secondRowStrings)
+            {
+                if (secondRowString.Contains(teams[0]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if (teams.Count > 1 && secondRowString.Contains(teams[1]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else if (teams.Count > 2 && secondRowString.Contains(teams[2]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                } 
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                Console.Write(secondRowString);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.Write(Environment.NewLine);
             Console.WriteLine(thirdRow);
             Console.WriteLine(fourthRow);
         }
