1a5aa7ccbcbfdc6dd7dd1c0272873c15a1613dc9
Lukas <lukas.raschke@web.de>
2015-06-12 18:35:06 +0200

    Team 5 Master strategy

--- RobotTournament/source/RobotEngine/Robot5.cs
+++ RobotTournament/source/RobotEngine/Robot5.cs
@@ -10,7 +10,9 @@
     {
         public NextRobotTurn DoNextTurn(int currentTurn, int myLevel, Surroundings environment)
         {
-            var splittingLimit = currentTurn < 20 ? 8 : currentTurn / 2;
+            var splittingLimit = currentTurn < 25 ? 8 : currentTurn * 2 / 3;
+
+            var attackLimit = currentTurn < 200 ? currentTurn * 2 : (int)(currentTurn * 1.4);
             
             var surroundingRobots = environment.Robots;
             var surroundingPowerUps = environment.PowerUps;
@@ -32,28 +34,48 @@
                     return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = strongestFriendAround.Direction };
                 }
 
-                var randomDirection = this.GetRandomDirections();
+                var strongestEnemyAround = strongerEnemies.OrderByDescending(e => e.Level).First();
+
+                var robotDirection = this.GetOppositeDirection(strongestEnemyAround.Direction);
                 var counter = 0;
-                while (strongerEnemies.Where(se => se.Direction == randomDirection).ToList().Any() && counter++ < 20)
+                while (strongerEnemies.Where(se => se.Direction == robotDirection).ToList().Any() && counter++ < 20)
                 {
-                    randomDirection = this.GetRandomDirections();
+                    robotDirection = this.GetRandomDirections();
                 }
 
-                return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = randomDirection };
+                return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = robotDirection };
             }
 
             if (surroundingPowerUps.Any())
             {
-                var maxPowerUp = surroundingPowerUps.OrderBy(p => p.Level).First();
+                var maxPowerUp = surroundingPowerUps.OrderByDescending(p => p.Level).First();
                 return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = maxPowerUp.Direction };
             }
-            
+
+            if (currentTurn > 30 && myLevel > attackLimit && new Random().Next(10) > 6)
+            {
+                var significantlyWeakerEnemies = enemies.Where(e => e.Level < myLevel * 0.8).ToList();
+                if (significantlyWeakerEnemies.Any())
+                {
+                    var strongestWeakerEnemy = significantlyWeakerEnemies.OrderByDescending(e => e.Level).First();
+                    return new NextRobotTurn() { NextAction = RobotActions.Moving, NextDirection = strongestWeakerEnemy.Direction };
+                }
+            }
+
             if (myLevel >= splittingLimit)
             {
+                var robotDirection = this.GetRandomDirections();
+
+                var counter = 0;
+                while (friends.Any(f => f.Direction == robotDirection) && counter++ < 20)
+                {
+                    robotDirection = this.GetRandomDirections();
+                }
+
                 return new NextRobotTurn()
                 {
                     NextAction = RobotActions.Splitting,
-                    NextDirection = this.GetRandomDirections()
+                    NextDirection = robotDirection
                 };
             }
 
@@ -63,6 +85,29 @@
         internal Directions GetRandomDirections()
         {
             return (Directions)new Random().Next(7);
+        }
+
+        internal Directions GetOppositeDirection(Directions direction)
+        {
+            switch (direction)
+            {
+                case Directions.N:
+                    return Directions.S;
+                case Directions.NE:
+                    return Directions.SW;
+                case Directions.E:
+                    return Directions.W;
+                case Directions.SE:
+                    return Directions.NW;
+                case Directions.S:
+                    return Directions.N;
+                case Directions.SW:
+                    return Directions.NE;
+                case Directions.W:
+                    return Directions.E;
+                default:
+                    return Directions.SE;
+            }
         }
     }
 }
